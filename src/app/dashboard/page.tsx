6260560e84b7c06df6ca377547d9bdf297daf6e4
Abner Santos <abner.kif1@gmail.com>
2023-08-28 14:17:38 -0300

    sign out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { HeaderDashBoard } from "@/components/Header/HeaderDashboard";
-import { BoxStatistics } from "@/components/dashboard/BoxStatistics";
 import { ButtonNewGroup } from "@/components/dashboard/ButtonNewGroup";
 import { ButtonNewPass } from "@/components/dashboard/ButtonNewPass";
 import { ButtonNewUser } from "@/components/dashboard/ButtonNewUser";
 import { GroupsBoxMobile } from "@/components/dashboard/GroupsBoxMobile";
-import { TableGroups } from "@/components/dashboard/TableGroups";
-import { TableUsers } from "@/components/dashboard/TableUsers";
 import { Tasks } from "@/components/dashboard/Tasks";
 import { TodoGroups } from "@/components/dashboard/TodosGroups";
 import { UsersBoxMobile } from "@/components/dashboard/UsersBoxMobile";
@@ -46,7 +43,7 @@ export default function Dashboard(){
     <div className="w-full grid grid-cols-1 gap-8 lg:h-screen">
       <div className="h-full flex relative lg:overflow-y-scroll py-4">
         <div className="w-full absolute flex flex-col gap-12 p-4 lg:pr-8 pb-12">
-          <HeaderDashBoard title="Dashboard" icon="home" />
+          <HeaderDashBoard title="Dashboard" />
           {role === 'Admin' && (
           <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="w-full bg-white rounded-lg shadow-lg flex flex-col gap-4 p-6">
@@ -79,7 +76,7 @@ export default function Dashboard(){
             <div className="w-full flex flex-col bg-white shadow-lg rounded-lg gap-4 p-6">
               <div className="w-full flex justify-between items-center gap-8">
                 <strong className="text-xl text-black font-bold">Grupos</strong>
-                <ButtonNewGroup refetch={refetch} />
+                <ButtonNewGroup refetch={refetchRepresentantes} />
               </div>
               <div className="w-full grid grid-cols-1 lg:grid-cols-4 gap-8">
                 {representantes !== undefined && representantes.map(item => (
--- src/components/Header/HeaderDashboard.tsx
+++ src/components/Header/HeaderDashboard.tsx
@@ -1,28 +1,22 @@
-import { AreaChart, CupSoda, Home, Store, Users } from "lucide-react";
+import { Home, LogOut } from "lucide-react";
+import Link from "next/link";
 
 interface Props{
   title: string;
-  icon: string;
 }
 
-export function HeaderDashBoard({ title, icon }: Props){
+export function HeaderDashBoard({ title }: Props){
+
   return(
-    <div className="flex flex-col gap-2">
-      <div className="flex items-center gap-2">
-        {icon === 'home' ? (
+    <div className="w-full flex items-center justify-between gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center gap-2">
           <Home className="w-4 h-4" color="rgb(106, 118, 138)" />
-        ) : icon === 'users' ? (
-          <Users className="w-4 h-4" color="rgb(106, 118, 138)" />
-        ) : icon === 'store' ? (
-          <Store className="w-4 h-4" color="rgb(106, 118, 138)" />
-        ) : icon === 'products' ? (
-          <CupSoda className="w-4 h-4" color="rgb(106, 118, 138)" />
-        ) : (
-          <AreaChart className="w-4 h-4" color="rgb(106, 118, 138)" />
-        )}
-        <strong className="text-sm font-normal text-black">| {title}</strong>
+          <strong className="text-sm font-normal text-black">| {title}</strong>
+        </div>
+        <strong className="text-xl font-bold text-black">{title}</strong>
       </div>
-      <strong className="text-xl font-bold text-black">{title}</strong>
+      <Link href="/api/logout" type='submit' className='w-fit px-4 py-2 bg-black text-white rounded-lg text-lg font-bold font-serif text-center'><LogOut className='w-6 h-6' color='white' /></Link>
     </div>
   )
 }
